62021c4c507a13cb5b6c2814d213a62f2ae09260
petern0411 <peternguyen0411@gmail.com>
2016-11-25 14:32:39 -0500

    validation

--- server/src/server.js
+++ server/src/server.js
@@ -3,46 +3,96 @@
 var express = require('express');
 //Creates and Express server.
 var app = express();
-var db = require('./database.js');
+var rd = require('./database.js');
+var StatusUpdateSchema = require('./schemas/statusupdate.json');
+var validate = require('express-jsonschema').validate;
+var wd = require('./database');
+var ad = require('./database');
+var bodyParser = require('body-parser');
+
+
 //defines what happens when it receives the 'GET /' request
 
-// app.use(bodyParser.text());
-
+app.use(bodyParser.text());
+app.use(bodyParser.json());
 //You run the server from 'server', so '..client/build' is 'server/../client/build'
 //',,' means "go up one directory"
 
+
+function postStatusUpdate(user, location, contents){
+  var time = new Date().getTime();
+  var newStatusUpdate = {
+    "lkeCounter":[],
+    "type": "StatusUpdate",
+    "contents":{
+      "author": user,
+      "postDate": time,
+      "location": location,
+      "contents": contents,
+      "likeCounter": []
+    },
+    "comments": []
+  };
+    newStatusUpdate = ad.addDocument('feedItems', newStatusUpdate);
+    var userData = rd.readDocument('users', user);
+    var feedData = rd.readDocument('feeds',userData.feed);
+    feedData.contents.unshift(newStatusUpdate._id);
+
+    //update the Feed object
+    wd.WRITEDocument('feeds', feedData);
+
+    //return the newly-posted onject
+    return newStatusUpdate;
+  }
+  app.post('.feeditem', validate({body: StatusUpdateSchema}), function(req,res){
+    var body = req.body;
+    var fromUser = getUserIdFromToken(req.get('Authorization'));
+
+    //check if the requester is authorized to post this update
+
+    if(fromUser === body.id){
+      var newUpdate = postStatusUpdate(body.userId, body.location, body.contents);
+      res.status(201);
+      res.set('Location', '/feeditem' + newUpdate._id);
+      // Send the update
+      res.send(newUpdate);
+    }
+    else{
+      res.status(401).end();
+    }
+  });
+
+
+
+
 app.use(express.static('../client/build'));
 
-//Starts the server on port 3000!
-app.listen(3000, function(){
-  console.log('Example app listening on port 3000');
-});
 
 
 /**
 * Resolves a feed item. Internal to the server, since it's synchronous.
 */
 function getFeedItemSync(feedItemId) {
-  var feedItem = db.readDocument('feedItems', feedItemId);
+  var feedItem = rd.readDocument('feedItems', feedItemId);
   // Resolve 'like' counter.
   feedItem.likeCounter = feedItem.likeCounter.map((id) =>
-  db.readDocument('users', id));
+  rd.readDocument('users', id));
   // Assuming a StatusUpdate. If we had other types of
   // FeedItems in the DB, we would
   // need to check the type and have logic for each type.
-  feedItem.contents.author = db.readDocument('users',
+  feedItem.contents.author = rd.readDocument('users',
   feedItem.contents.author);
   // Resolve comment author.
   feedItem.comments.forEach((comment) => {
-    comment.author = db.readDocument('users', comment.author);
+    comment.author = rd.readDocument('users', comment.author);
   });
   return feedItem;
 }
 
 
 function getFeedData(user) {
-  var userData = db.readDocument('users', user);
-  var feedData = db.readDocument('feeds', userData.feed);
+  var userData = rd.readDocument('users', user);
+  var feedData = rd.readDocument('feeds', userData.feed);
   // While map takes a callback, it is synchronous,
   // not asynchronous. It calls the callback immediately.
   feedData.contents = feedData.contents.map(getFeedItemSync);
@@ -92,4 +142,17 @@ app.get('/user/:userid/feed', function(req, res) {
     // 401: Unauthorized request.
     res.status(401).end();
   }
+});
+
+app.use(function(err,req,res,next){
+  if(err.name ==='JsonSchemaValidation'){
+    res.status(400).end();
+  }else{
+    next(err);
+  }
+});
+
+
+app.listen(3000, function(){
+  console.log('Example app listening on port 3000');
 });
